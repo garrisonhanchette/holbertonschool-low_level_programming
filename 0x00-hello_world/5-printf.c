51a399c3fc95cd4e0e1f3a3d2fb5995796de0025
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-15 11:52:32 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+nt main(void)
+{
+	        printf "\n%s\n" "\"Programming is like building a multilingual puzzle");
+		        return (0);
+}i
