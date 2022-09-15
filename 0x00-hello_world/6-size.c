f7d211b2c77f1e9ca08e9a0a32636c8806c0a773
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-15 12:47:21 -0700

    printing size of types on computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main -Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int integerType;
+	float floatType;
+	long int longintType;
+	long long int longlongintType;
+	char charType;
+		printf("Size of char: %ld bytes\n",sizeof(charType));
+		printf("Size of int: %ld bytes\n",sizeof(intType));
+		printf("Size of long int: %ld bytes\n",sizeof(longintType));
+		printf("Size of long long int: %ld byte\n",sizeof(longlongintType));
+		printf("Size of float: %ld byte\n", sizeof(floatType));
+}
