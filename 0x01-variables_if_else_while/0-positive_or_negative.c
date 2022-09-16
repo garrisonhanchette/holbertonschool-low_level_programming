c9aac78dc8b87de16e5884a41050c94bd1f79b69
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-16 08:57:14 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0.0)
-		printf("is positive\n");
+		printf("is positive\n",n);
 	else if (n < 0.)
-		printf("is negative\n");
+		printf("is negative\n",n);
 	else
-		printf("is zero\n");
+		printf("%i is zero\n",n);
 	return (0);
 }
