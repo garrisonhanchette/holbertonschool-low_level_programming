f6df3cc184672af607846fe9c443b0b3d3216ec4
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-20 08:59:22 -0700

    alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' && x != 'e')
+	{
+		putchar(x);
+	}
+	{
+		putchar('\n');
+	}
+return (0);
+}
