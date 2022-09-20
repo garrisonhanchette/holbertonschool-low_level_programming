06ef11256fbdb9eb5248ad37ffdb3bd3f48e234f
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-20 09:10:45 -0700

    alphabet except p and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,10 @@ int main(void)
 char x;
 for (x = 'a'; x <= 'z'; x++)
 	{
-		if (x != 'q' && x != 'e')
-			{
-			putchar(x);
-			}
+	if (x != 'q' && x != 'e')
+		{
+		putchar(x);
+		}
 		{
 		putchar('\n');
 		}
