deced76d0adfd709214e06eadfa7e7da3b9f356b
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-20 08:31:45 -0700

    upper and lowercase alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,13 @@
 int main(void)
 {
 char x;
-	for (x = 'a'; x <= 'z'; x++ | x = 'A'; x <= 'Z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
+	for (x = 'A'; x <= 'Z'; x++)
 	{
+		putchar (x);
 		putchar('\n');
 	}
 return (0);
