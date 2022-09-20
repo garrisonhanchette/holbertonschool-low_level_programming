60af07267d9a7da0949d325322cc98748e9b6d22
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-20 08:24:54 -0700

    print alphabet upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char x;
+	for (x = 'a'; x <= 'z'; x++ | x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	{
+		putchar('\n');
+	}
+return (0);
+}
