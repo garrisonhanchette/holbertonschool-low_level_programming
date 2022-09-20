7a6602c246c6f13990631094717f6f313c9698bf
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-20 08:07:56 -0700

    printing alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <studio.>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char x;
+	for (x='a'; x<='z'; x++)
+	{
+		putchar(x);
+	}
+	{
+		putchar('\n');
+	}
+return(0);
+}
