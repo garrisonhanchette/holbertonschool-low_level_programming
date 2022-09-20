3de20ecc972045b7d681667469b685717309e83f
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-20 12:32:57 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char x;
+	for (x = 'z'; x <= 'a'; x++)
+	{
+		putchar(x)
+	}
+	{
+		putchar('\n');
+	}
+return (0);
+}
