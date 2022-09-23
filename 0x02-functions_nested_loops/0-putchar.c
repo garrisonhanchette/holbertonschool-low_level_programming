075d76b7d7db8e6778635064b22a9c60b346d0d2
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-23 11:46:23 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * main - Check description
- * Description: It prints _putchar, followed by a new line.
- * Return: 0.
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char word[9] = "_putchar";
-	int i;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
-	_putchar('\n');
-
-	return (0);
+return (0);
 }
