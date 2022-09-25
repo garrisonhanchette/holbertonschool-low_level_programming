9ac58cc15de8216f640edc9a31967c6dcf2ec898
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-09-24 21:39:22 -0700

    void print alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,12 @@
 void print_alphabet_x10(void)
 {
 	int x;
+
 	for (x = 0; x < 10; x++)
 	{
 		int i;
-		for (i = 97; i <=122; i++)
+
+		for (i = 97; i <= 122; i++)
 			_putchar(i);
 		_putchar('\n');
 	}
