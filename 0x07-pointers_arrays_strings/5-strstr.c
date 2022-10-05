069d7b925d4d5054e020baaeadb74cd444460286
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-10-04 22:00:29 -0700

    five

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * _strstr - Locates a matching substring within a string
+ *
+ * @haystack: Where we're searching
+ * @needle: What we're looking for
+ *
+ * Return: Entire string starting where substring matched
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	const char *a;
+	const char *b;
+
+	b = needle;
+
+	if (*b == 0)
+	{
+		return (haystack);
+	}
+
+	for ( ; *haystack != 0; haystack++)
+	{
+		if (*haystack != *b)
+		{
+			continue;
+		}
+		a = haystack;
+		while (1)
+		{
+			if (*b == 0)
+			{
+				return (haystack);
+			}
+			if (*a++ != *b++)
+			{
+				break
+			}
+			b = needle;
+		}
+		return (NULL);
+}
