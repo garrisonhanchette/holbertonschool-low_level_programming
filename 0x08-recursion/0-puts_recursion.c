b26ec17cb1e59689671ef21e3555b8eda114186c
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-10-12 15:08:43 -0700

    print string followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Prints the given string followed by new line
+ * @s: Provided string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
