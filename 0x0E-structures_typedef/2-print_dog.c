e9115a333b2a6e865e9ce286c303162a215d4a71
garrisonhanchette <garrisonhanchette24@gmail.com>
2022-10-25 20:53:56 -0700

    commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints struct dog
+ * @d: struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			d->name = "(nil)";
+
+		if (d->owner == NULL)
+			d->owner = "(nil)";
+
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
