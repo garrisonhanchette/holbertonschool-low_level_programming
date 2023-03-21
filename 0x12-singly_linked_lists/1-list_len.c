ee862cd56897e4babf1ef0aeaafc89a3e2df14d3
garrisonhanchette <garrisonhanchette24@gmail.com>
2023-03-20 21:59:52 -0700

    m

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,32 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * list_len - Function that prints all elements of a list
+ * @h: pointer to head of singly linked list
  *
- * Return: number of nodes
+ * Return: Number of elements in list
  */
-size_t listint_len(const listint_t *h)
+
+size_t list_len(const list_t *h)
 {
- size_t num = 0;
+	const list_t *temp = h;
+	unsigned int i = 0;
 
- while (h)
- {
- num++;
- h = h->next;
- }
-
- return (num);
+	while (temp)
+	{
+		if (temp->str == NULL)
+		{
+			temp = temp->next;
+			i++;
+		}
+		else
+		{
+			temp = temp->next;
+			i++;
+		}
+	}
+	return (i);
 }
-
